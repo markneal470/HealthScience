4c15cd969fc0d86fc2ddd4a15f9ae366f0c0cc41
markneal470 <74790459+markneal470@users.noreply.github.com>
2021-01-29 11:32:00 -0600

    E

---
+++ HealthScience/app-files/data.js
@@ -0,0 +1,67 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-healthscience",
+      "name": "HealthScience",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.9420899212855698,
+          "pitch": 0.15141502435130505,
+          "title": "Needles and Equipment",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.637186659638063,
+          "pitch": 0.359828718713084,
+          "title": "Class Expectations",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.4028020999843065,
+          "pitch": 0.3194539425106768,
+          "title": "Handwashing",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.31036564691833135,
+          "pitch": 0.12948805658340845,
+          "title": "Introduction",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
